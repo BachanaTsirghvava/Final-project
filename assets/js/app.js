1466039af7f0f4353fbd125ecc52e0b971f69790
BachanaTsirghvava <123394439+BachanaTsirghvava@users.noreply.github.com>
2023-04-03 17:36:17 +0400

    add few changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -65,3 +65,57 @@ window.addEventListener("scroll", function() {
 
     scrollTopCheck(scroll_top);
 });
+
+//modal 
+const modal = document.querySelector('.modal-wrapper')
+const closeBtn = document.querySelector('.modal-close')
+const openBtn = document.querySelector('.modal-open')
+
+
+
+openBtn.addEventListener('click', () => {
+    triggerModal(modal)
+})
+closeBtn.addEventListener('click', () => {
+    triggerModal(modal)
+})
+
+
+window.addEventListener('click', (e) => {
+    if(e.target == document.querySelector('.modal-wrapper')) {
+        triggerModal(modal)
+    }
+})
+
+
+function triggerModal (modal) {
+    modal.classList.toggle('active')
+}
+
+// carousel
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
