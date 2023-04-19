5825d8949ee75d155f3d14f469cb03c2c5f892d2
BachanaTsirghvava <123394439+BachanaTsirghvava@users.noreply.github.com>
2023-04-19 20:52:18 +0400

    final changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,19 @@
 const navBurger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
 const nav = document.querySelector(".nav");
-const searchForm = document.querySelector(".search-form");
-const container = document.querySelector(".container");
-const productBurger = document.querySelector(".product-burger");
-const productLinks = document.querySelector(".product-ul");
 
 function navActiveToggler() {
     navLinks.classList.toggle("nav-links__active");
     navBurger.classList.toggle("nav-burger__active");
     nav.classList.toggle("nav__active");
-    searchForm.classList.toggle("search-form__active");
-    container.classList.toggle("active-container");
 }
 
 
 navBurger.addEventListener("click", navActiveToggler);
 
-/*
-function productActiveToggler() {
-    productBurger.classList.toggle("product-burger__active");
-    productLinks.classList.toggle("produc-ul__active");
-}
-
-productBurger.addEventListener("click", productActiveToggler); */
 
 const navIndicator = document.getElementById("nav-indicator"); 
 const scrollTopButton = document.getElementById("scroll-top");
-
-//function which handles nav indicator size
- function navIndicatorHandler(){
-    let winScroll = document.documentElement.scrollTop;
-    let height = 
-    document.documentElement.scrollHeight - 
-    document.documentElement.clientHeight;
-
-    let scrolled = Math.floor((winScroll / height) * 100);
-
-    console.log(scrolled);
-
-    navIndicator.style.width = scrolled + "%";
-} 
-
-//events
-window.addEventListener("scroll", navIndicatorHandler);
 
 
 //function which handles scroll top 
